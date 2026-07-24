8c36e3c2771e9c40e1699cb0e24fb75307cd4be1
Vale Rojas <valerojasu98@hotmail.com>
2026-07-24 13:02:14 -0600

    Update portfolio routing

--- src/Components/Paginas/Home.js
+++ src/Components/Paginas/Home.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import './Home.css';
-import '../Navegacion/Navb.css';
-import Bio from './Bio';
-import Conocimientos from '../Paginas/Conocimientos';
-import Work from '../Paginas/Work';
-import Formulario from './Formulario';
-import Footerp from '../Paginas/Footerp';
-import HomeNavbar from '../Navegacion/HomeNavbar'; // ✅ navbar importado
+import React from "react";
+import "./Home.css";
+import "../Navegacion/Navb.css";
+import Bio from "./Bio";
+import Conocimientos from "../Paginas/Conocimientos";
+import Work from "../Paginas/Work";
+import Formulario from "./Formulario";
+import Footerp from "../Paginas/Footerp";
+import HomeNavbar from "../Navegacion/HomeNavbar"; // ✅ navbar importado
 
 const Home = () => {
   return (
     <div>
-      <div className='portada'>
-        <HomeNavbar /> 
+      <div className="portada">
+        <HomeNavbar />
         <h1 className="portatitu">
-          <strong>HEY, HOLA!</strong><br />
-          Bienvenido a mi portafolio
+          <strong>Hi, I'm Valeria Rojas</strong>
+          <br />
+          FULL STACK DEVELOPER
+          <br />I build modern and responsive websites and web applications.
         </h1>
       </div>
 
