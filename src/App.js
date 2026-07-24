bd4a4ef350e9a2e2d8b01c949a855ac402ff74f6
Vale Rojas <valerojasu98@hotmail.com>
2026-07-24 12:58:54 -0600

    Fix Netlify routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './Components/Paginas/About';
-import Mainp from './Components/Paginas/Mainp';
-import Home from './Components/Paginas/Home';
-import Navb from './Components/Navegacion/Navb';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import About from "./Components/Paginas/About";
+import Home from "./Components/Paginas/Home";
+import Navb from "./Components/Navegacion/Navb";
 
 const App = () => {
-  return ( 
-<Router>
-<Routes>
-<Route path="/navb" element={<Navb />}/>
-<Route path="/" element={<Mainp />} />
-<Route path="/mainp" element={<Mainp />} />
-<Route path="/home" element={<Home />} />
-<Route path="/about" element={<About />} />
-</Routes>
-</Router>
+  return (
+    <Router>
+      <Navb />
 
+      <Routes>
+        {/* Página principal */}
+        <Route path="/" element={<Home />} />
+
+        {/* Página Home */}
+        <Route path="/home" element={<Home />} />
+
+        {/* Página About */}
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
