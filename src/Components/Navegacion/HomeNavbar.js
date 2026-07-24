a8b55a536cd3e425e95671c9ff7ab3a56a591027
Vale Rojas <valerojasu98@hotmail.com>
2026-07-24 11:51:15 -0600

    200

--- src/Components/Navegacion/HomeNavbar.js
+++ src/Components/Navegacion/HomeNavbar.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import './HomeNavbar.css';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./HomeNavbar.css";
 import v from "../../assets/V/v6.png";
 
 const HomeNavbar = () => {
@@ -11,24 +11,33 @@ const HomeNavbar = () => {
     const handleScroll = () => {
       setNavScrolled(window.scrollY > 50);
     };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-    <div className={`navbar-home ${navScrolled ? 'scrolled' : ''}`}>
+    <div className={`navbar-home ${navScrolled ? "scrolled" : ""}`}>
       <div className="nav-container">
-        <Link to='/Mainp'>
-          <img src={v} alt="logo" className='logov' />
+        <Link to="/Mainp">
+          <img src={v} alt="logo" className="logov" />
         </Link>
 
-        <div className={`nav-links ${menuOpen ? 'active' : ''}`}>
-          <Link className="menutinav" to='/about' onClick={() => setMenuOpen(false)}>About</Link>
+        <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+          <Link
+            className="menutinav"
+            to="/about"
+            onClick={() => setMenuOpen(false)}
+          >
+            About
+          </Link>
         </div>
 
-        <div className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div
+          className={`hamburger ${menuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
           <span></span>
           <span></span>
           <span></span>
