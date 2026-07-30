294b40a29db52a96ed1e5531986ca0ca4d8f730a
Vale Rojas <valerojasu98@hotmail.com>
2026-07-29 19:16:50 -0600

    NAV

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import About from "./Components/Paginas/About";
 import Home from "./Components/Paginas/Home";
-import Navb from "./Components/Navegacion/Navb";
 
 const App = () => {
   return (
